b6c9797ae2f9ad07ef0b51299eef5d98b2b3cf8c
Vyom Singh <112750331+vyom198@users.noreply.github.com>
2023-12-11 20:28:28 +0530

    minor-fix

--- app/src/main/java/com/myapp/alarm/data/di/DataModule.kt
+++ app/src/main/java/com/myapp/alarm/data/di/DataModule.kt
@@ -14,6 +14,7 @@ import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
+
 @Module
 @InstallIn (SingletonComponent::class)
 object DataModule {
